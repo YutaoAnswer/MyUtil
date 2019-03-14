f40cf69012b62a8dbe4b026d248245877afd8046
YutaoAnswer <zhongyut1994@qq.com>
2019-03-14 11:19:53 +0800

    add one file: SDKVersionUtil

---
+++ app/src/main/java/com/yutao/myutil/SDKVersionUtil.java
@@ -0,0 +1,17 @@
+package com.yutao.myutil;
+
+public class SDKVersionUtil {
+
+    /**
+     * 获取SDK版本
+     */
+    public static int getSDKVersion() {
+        int version = 0;
+        try {
+            version = Integer.valueOf(android.os.Build.VERSION.SDK);
+        } catch (NumberFormatException e) {
+        }
+        return version;
+    }
+
+}
